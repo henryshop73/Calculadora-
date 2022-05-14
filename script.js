08e3c4f5297c71d319315ec098ed99076cea5a1b
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-13 23:04:25 -0500

    Segundo commit

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-function suma() {
+function calcular(operacion) {
     let num1 = document.getElementById("num1").value
     let num2 = document.getElementById("num2").value
 
-    let suma =parseInt(num1) + parseInt(num2)
-    alert("El resultado es: " + suma)
+    let resultado
+
+    switch (operacion) {
+        case "suma":
+            resultado =  parseInt(num1) + parseInt(num2)
+            break;
+        case "resta":
+            resultado = parseInt(num1) - parseInt(num2)
+            break
+        case "multiplicacion":
+            resultado = parseInt(num1) * parseInt(num2)
+            break
+        case "dividir":
+            resultado = parseInt(num1) / parseInt(num2)
+            break
+    
+        default:
+            break;
+    }
+    alert("El resultado es:" + resultado)
 }
+
+
 
 function resta() {
     let num1 = document.getElementById("num1").value
