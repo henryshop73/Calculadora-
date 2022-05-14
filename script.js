fd74d96f25bfea184a7e98172831d55b029165a6
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-13 23:07:51 -0500

    Eliminar funciones que noi se usan

--- script.js
+++ script.js
@@ -23,29 +23,3 @@ function calcular(operacion) {
     }
     alert("El resultado es:" + resultado)
 }
-
-
-
-function resta() {
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
-
-    let resta =parseInt(num1) - parseInt(num2)
-    alert("El resultado es: " + resta)
-}
-
-function multiplicacion() {
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
-
-    let multiplicar = parseInt(num1) * parseInt(num2)
-    alert("El resultado es: " + multiplicar)
-}
-
-function dividir() {
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
-
-    let division = parseInt(num1) / parseInt(num2)
-    alert("El resultado es:" + division)
-}
