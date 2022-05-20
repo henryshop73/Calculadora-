5d6fd617a283d79c8547e9713d1941babd7e9044
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-19 22:55:33 -0500

    Eventos desde HTML

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-function calcular(operacion) {
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
-
-    let resultado
-
-    switch (operacion) {
-        case "suma":
-            resultado =  parseInt(num1) + parseInt(num2)
-            break;
-        case "resta":
-            resultado = parseInt(num1) - parseInt(num2)
-            break
-        case "multiplicacion":
-            resultado = parseInt(num1) * parseInt(num2)
-            break
-        case "dividir":
-            resultado = parseInt(num1) / parseInt(num2)
-            break
-    
-        default:
-            break;
-    }
-    alert("El resultado es: " + resultado)
+function actionButtons() {
+    let buttonClose = document.getElementById("buttonClose")
+    buttonClose.classList.add("text-color-black")
+}
+function disableActionButtons() {
+    let buttonClose = document.getElementById("buttonClose")
+    buttonClose.classList.remove("text-color-black")
 }
