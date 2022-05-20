39a2e6ff369f94747402c13402de0e1321abe657
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-19 23:21:30 -0500

    Eventos desde JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function actionButtons() {
+
+let actionButtons = document.getElementById("actionsButtons")
+actionButtons.addEventListener("mouseover", activeButtons )
+actionButtons.addEventListener("mouseout", disableActionButtons)
+
+
+function activeButtons() {
     let buttonClose = document.getElementById("buttonClose")
     buttonClose.classList.add("text-color-black")
 }
