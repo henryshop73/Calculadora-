b5dfb5dcb40579b680eddac40e0d42cf91d7dcac
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-20 20:03:42 -0500

    Añadiendo Eventos a  los botones Min y Max desde JavaScript

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 let actionButtons = document.getElementById("actionsButtons")
 actionButtons.addEventListener("mouseover", activeButtons )
 actionButtons.addEventListener("mouseout", disableActionButtons)
+actionButtons.addEventListener("mouseover", activeButtonsMin)
+actionButtons.addEventListener("mouseout", disableActionButtonsMin)
+actionButtons.addEventListener("mouseover", activeButtonMax)
+actionButtons.addEventListener("mouseout", disableButtonMax)
 
 
 function activeButtons() {
@@ -11,4 +15,22 @@ function activeButtons() {
 function disableActionButtons() {
     let buttonClose = document.getElementById("buttonClose")
     buttonClose.classList.remove("text-color-black")
+}
+
+function activeButtonsMin() {
+    let buttonMin = document.getElementById("buttonMin")
+    buttonMin.classList.add("text-color-black")
+}
+function disableActionButtonsMin() {
+    let buttonMin = document.getElementById("buttonMin")
+    buttonMin.classList.remove("text-color-black")
+}
+
+function activeButtonMax() {
+    let buttonMax = document.getElementById("buttonMax")
+    buttonMax.classList.add("text-color-black")
+}
+function disableButtonMax() {
+    let buttonMax = document.getElementById("buttonMax")
+    buttonMax.classList.remove("text-color-black")
 }
