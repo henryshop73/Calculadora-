023d642856c96ff31486bc1460726c9fe0176ad7
Henry Moran <henrymoranfreelance@gmail.com>
2022-05-20 21:34:25 -0500

    Refactorizando codigo de eventos en JS

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 
 let actionButtons = document.getElementById("actionsButtons")
-actionButtons.addEventListener("mouseover", activeButtons )
 actionButtons.addEventListener("mouseout", disableActionButtons)
-actionButtons.addEventListener("mouseover", activeButtonsMin)
-actionButtons.addEventListener("mouseout", disableActionButtonsMin)
-actionButtons.addEventListener("mouseover", activeButtonMax)
-actionButtons.addEventListener("mouseout", disableButtonMax)
+actionButtons.addEventListener("mouseover", enableActionButons)
+let buttonClose = document.getElementById("buttonClose")
+let buttonMin = document.getElementById("buttonMin")
+let buttonMax = document.getElementById("buttonMax")
 
 
-function activeButtons() {
-    let buttonClose = document.getElementById("buttonClose")
+
+function enableActionButons() {
     buttonClose.classList.add("text-color-black")
-}
-function disableActionButtons() {
-    let buttonClose = document.getElementById("buttonClose")
-    buttonClose.classList.remove("text-color-black")
-}
-
-function activeButtonsMin() {
-    let buttonMin = document.getElementById("buttonMin")
     buttonMin.classList.add("text-color-black")
-}
-function disableActionButtonsMin() {
-    let buttonMin = document.getElementById("buttonMin")
-    buttonMin.classList.remove("text-color-black")
-}
-
-function activeButtonMax() {
-    let buttonMax = document.getElementById("buttonMax")
     buttonMax.classList.add("text-color-black")
 }
-function disableButtonMax() {
-    let buttonMax = document.getElementById("buttonMax")
+
+function disableActionButtons() {
+    buttonClose.classList.remove("text-color-black")
+    buttonMin.classList.remove("text-color-black")
     buttonMax.classList.remove("text-color-black")
 }
